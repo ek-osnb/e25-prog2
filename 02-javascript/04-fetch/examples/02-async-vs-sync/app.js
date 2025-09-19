6efe148f940dd6712e5bbe1b6bcff632686e783d
osman-butt <butt.osman@gmail.com>
2025-09-19 12:17:06 +0200

    example solution

--- 02-javascript/04-fetch/examples/02-async-vs-sync/app.js
+++ 02-javascript/04-fetch/examples/02-async-vs-sync/app.js
@@ -1,3 +1,24 @@
 // TODO: Create slow tasks -> show tasks
 // TODO: Create async tasks
 
+document.querySelector("button").addEventListener("click", (e) => {
+    e.target.style.backgroundColor = "red";
+});
+
+
+
+// function wait(ms) {
+//     const start = Date.now();
+//     while ((Date.now() - start) < ms) {
+
+//     }
+//     console.log("wait finished!")
+// }
+
+
+// wait(10_000);
+
+
+setTimeout(() => {
+    console.log("FINISHED ASYNC WORK");
+}, 5_000);

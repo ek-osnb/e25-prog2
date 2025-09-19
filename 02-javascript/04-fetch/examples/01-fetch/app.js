8ccaae357eb69ac7b5300edf89eb0eceefa462e8
osman-butt <butt.osman@gmail.com>
2025-09-19 09:58:36 +0200

    finished example

--- 02-javascript/04-fetch/examples/01-fetch/app.js
+++ 02-javascript/04-fetch/examples/01-fetch/app.js
@@ -1,15 +1,96 @@
 window.addEventListener('DOMContentLoaded', initApp);
 
-// TODO: CREATE A GET Request
-// https://jsonplaceholder.typicode.com/users
+// TODO: PRETTIER
 
-// TODO: HEADERS
-// TODO: Return data to console
-// TODO: Return an object
-// TODO: Use Async/Await
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+const USERS_URL = `${BASE_URL}/users`;
+const POSTS_URL = `${BASE_URL}/posts`;
 
-// TODO: CREATE A POST REQUEST
-
-function initApp() {
+async function initApp() {
     // Code goes here
+    console.log(document.querySelector("h1").textContent);
+    // getUsersAsync().then(u => displayUsers(u));
+    const users = await getUsersAsync();
+    displayUsers(users);
+
+    const newPost = await createPost({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    });
+    console.log(newPost);
+
+    const newUser = await createUser({
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+    });
+    console.log(newUser);
 }
+
+function displayUsers(users) {
+    users.forEach(u => displayUser(u));
+}
+
+function displayUser(user) {
+    const ul = document.querySelector("#userList");
+    const li = document.createElement("li");
+    li.textContent = `${user.name} - ${user.email}`;
+    ul.appendChild(li);
+}
+
+function getUsers() {
+    return fetch(USERS_URL).then(resp => {
+        if (!resp.ok) {
+            console.log("Status code: " + resp.status)
+        }
+        return resp.json();
+    })
+    // .then(users => displayUsers(users));
+}
+
+async function getUsersAsync() {
+    const resp = await fetch(USERS_URL);
+    if (!resp.ok) {
+        console.log("Status code: " + resp.status)
+    }
+    return await resp.json();
+    // .then(users => displayUsers(users));
+}
+
+
+async function createPost(post) {
+    const resp = await fetch(POSTS_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(post)
+    })
+
+    if (!resp.ok) {
+        console.log("ERROR: " + resp.status)
+        console.log("ERROR: " + resp.statusText)
+    }
+
+    return await resp.json();
+}
+
+async function createUser(user) {
+    const resp = await fetch(USERS_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+
+    if (!resp.ok) {
+        console.log("ERROR: " + resp.status)
+        console.log("ERROR: " + resp.statusText)
+    }
+
+    return await resp.json();
+}
+
+
